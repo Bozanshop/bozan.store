e0f64875f1a1febb6fa2d20e207afbc1f961da9f
Bozan <bozanweb@gmail.com>
2025-04-17 23:15:57 +0200

    Opraveno: layout.tsx a typy params

--- app/ms/layout.tsx
+++ app/ms/layout.tsx
@@ -1,12 +1,9 @@
-import { ReactNode } from "react";
-
-export default async function Layout({
-  children,
-  params,
-}: {
-  children: ReactNode;
+export default async function Layout(props: {
+  children: React.ReactNode;
   params: { locale: string };
 }) {
+  const { children, params } = props;
+
   return (
     <html lang={params.locale}>
       <body>{children}</body>
